f3339664d0a4bf7cca8420357f869d258da5d17d
xiangju-y <2743103508@qq.com>
2024-04-16 08:45:12 +0800

    Site updated: 2024-04-16 08:45:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/09/29/python学习中的笔记/","2021/09/20/记录一次小米路由器4c刷机过程/"];function toRandomPost(){
+var posts=["2024/04/16/笔记/linux/shell/","2024/04/16/笔记/linux/nfs/","2024/04/16/笔记/linux/ansible/","2022/09/29/python学习中的笔记/","2021/09/20/记录一次小米路由器4c刷机过程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
